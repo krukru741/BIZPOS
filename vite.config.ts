c20fa803e258efd2c0ec54a4a3d8275de3ec7880
Alben Gacayan <albengacayan486@gmail.com>
2026-08-12 15:09:36 +0800

    chore: upgrade to Tailwind CSS v4 and integrate its Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron/simple'
 
+import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     electron({
       main: {
