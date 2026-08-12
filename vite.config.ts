75c780bfec57c3a28f8feba9883047bc9b595567
Alben Gacayan <albengacayan486@gmail.com>
2026-08-12 15:07:02 +0800

    chore: remove tailwindcss plugin from vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron/simple'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(),
     react(),
     electron({
       main: {
