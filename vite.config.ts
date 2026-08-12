33189afc15dc6fca3c2067fbd1226e52aa02f3ab
Alben Gacayan <albengacayan486@gmail.com>
2026-08-12 14:06:05 +0800

    feat: integrate Tailwind CSS Vite plugin into build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import electron from 'vite-plugin-electron/simple'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react(),
     electron({
       main: {
